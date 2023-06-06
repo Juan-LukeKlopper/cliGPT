523739ca8723031f89653b4c061d2878ad413a5d
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-06-06 14:13:43 +0200

    feat: Take arg and send it to chatGPT as a prompt

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use llm_chain::{executor, parameters, prompt};
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    prompt: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
 }
+
+// Declare an async main function
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Create a new ChatGPT executor
+    let exec = executor!()?;
+
+    // Define the command-line interface using Clap
+    let args = Args::parse();
+
+    // Create our prompt...
+    let res = prompt!(
+        "You are chatGPT",
+        &args.prompt
+    )
+    .run(&parameters!(), &exec) // ...and run it
+    .await?;
+    println!("{}", res);
+
+    Ok(())
+}
+
